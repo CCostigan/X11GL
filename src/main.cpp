ec858b0cb53335c6e4c9ea9887e5f1e04cc4d98b
I am Groot <groot@localhost.com>
2022-04-09 10:44:31 -0400

    Moved main, added some files

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include <X11GL.h>
+#include "X11GL/X11GL.h"
 
 int main(int argc, char** argv) {
   
---
+++ src/x11gl/X11GL.h
@@ -0,0 +1,36 @@
+
+#ifndef _XSTUFF_HPP
+#define _XSTUFF_HPP
+
+
+#ifndef __APPLE__
+    //#include <X11/XF86keysym.h>
+    #include <X11/Xlib.h>
+    #include <X11/Xutil.h>
+    #include <X11/Xos.h>
+
+    #define GL_GLEXT_PROTOTYPES
+    #include <GL/glx.h>
+    //#include <GL/glxew.h>
+    #include <GL/glxext.h>
+    //#include <GL/glxint.h>
+#endif
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
+class X11GL {
+
+    public:
+        X11GL();
+        virtual ~X11GL();
+        void init(std::string, int, float);
+        int render();
+};
+
+
+#endif //_XSTUFF_HPP
+
